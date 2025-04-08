e6ccb487396e2f50668641207b1f861b8d28a17c
id-kuznetsov <ilua.kuznetsov@gmail.com>
2025-04-08 22:06:25 +0300

    Initial Commit

---
+++ CryptoWallet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CryptoWallet
+//
+//  Created by Ilya Kuznetsov on 08.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
